81b3bda660a2e26cbaa2645f2323275459d8f0b4
franca <francavisokolskis@gmail.com>
2022-08-22 17:08:16 -0300

    seteo base

---
+++ api/models/index.js
@@ -0,0 +1,9 @@
+//const Users = require("./Users");
+
+
+/* Users.belongsToMany(Movies, { as: "favorites", through: "favorites_movies" });
+Movies.belongsToMany(Users, { as: "favorites", through: "favorites_movies" }); */
+
+
+
+//module.exports = {};
