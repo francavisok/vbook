6aeef521afb798b300aee49f48b8878be6a237cc
franca <francavisokolskis@gmail.com>
2022-08-23 15:16:12 -0300

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,50 +1,64 @@
 import React from "react";
 
-import { useForm } from 'react-hook-form'
+import { useForm } from "react-hook-form";
 import {
   FormErrorMessage,
   FormLabel,
   FormControl,
   Input,
+  InputGroup,
   Button,
-} from '@chakra-ui/react'
+  InputLeftAddon,
+  Stack,
+  Icon,
+  InputLeftElement,
+} from "@chakra-ui/react";
 
 const Login = () => {
-    const {
-        handleSubmit,
-        register,
-        formState: { errors, isSubmitting },
-      } = useForm()
-    
-      function onSubmit(values) {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            resolve()
-          }, 3000)
-        })
-      }
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  function onSubmit(values) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        resolve();
+      }, 3000);
+    });
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-    <FormControl isInvalid={errors.name}>
-      <FormLabel htmlFor='name'>First name</FormLabel>
-      <Input
-        id='name'
-        placeholder='name'
-        {...register('name', {
-          required: 'This is required',
-          minLength: { value: 4, message: 'Minimum length should be 4' },
-        })}
-      />
-      <FormErrorMessage>
-        {errors.name && errors.name.message}
-      </FormErrorMessage>
-    </FormControl>
-    <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
-      Submit
-    </Button>
-  </form>
+      <Stack m={9}>
+        <FormControl isInvalid={errors.name}>
+          <InputGroup>
+            <InputLeftElement children={<Icon name="info" />} />
+            <Input
+              id="email"
+              placeholder="email"
+              {...register("email", {
+                required: "This is required",
+                minLength: { value: 4, message: "Minimum length should be 4" },
+              })}
+            />
+          </InputGroup>
+          <FormErrorMessage>
+            {errors.name && errors.name.message}
+          </FormErrorMessage>
+        </FormControl>
+        <Button
+          mt={4}
+          colorScheme="teal"
+          isLoading={isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Stack>
+    </form>
   );
 };
 
