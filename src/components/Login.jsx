53872a2f80a3ff5f6f16455772a15fd26c291f8f
franca <francavisokolskis@gmail.com>
2022-08-23 17:32:38 -0300

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-
-
+import {
+  Container,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+} from "@chakra-ui/react";
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
 
 const Login = () => {
-
-
   return (
-    <div></div>
+    <Container  maxW='xl' bg='lightgray' centerContent  >
+      <form>
+        <Stack>
+          <Heading as={"h2"} size="lg">
+            Login
+          </Heading>
+          <InputGroup>
+            <InputLeftElement children={<EmailIcon />} />
+            <Input type={"email"} placeholder="Email" />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement children={<LockIcon />} />
+            <Input type={"password"} placeholder="Password" />
+          </InputGroup>
+        </Stack>
+      </form>
+    </Container>
   );
 };
 
