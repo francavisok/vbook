c9d9144bb1b45fb1e4e184813403d6360f80a091
Marcos Facundo Rueda Adanto <marcos.fac.rueda@gmail.com>
2022-08-23 19:39:23 -0300

    build Register form with initial values

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<div>MAIN</div>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<div>REGISTER</div>} />
+        <Route path="/register" element={<Register/>} />
         <Route path="/cart" element={<div>CART</div>} />
         <Route path="/search" element={<div>SEARCH</div>} />
         <Route path="/category/:genre" element={<div>GENRE</div>} />
