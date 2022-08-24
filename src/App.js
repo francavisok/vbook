da6d8228b8417a283c0265c5de616bf6f90ea0d7
franca <francavisokolskis@gmail.com>
2022-08-24 13:42:22 -0300

    padding para todos las vistas de main

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 
 import Footer from "./components/Footer";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import Register from "./components/Register";
-
 
 function App() {
   return (
-    <Flex direction={'column'} minHeight='100vh'>
+    <Flex direction={"column"} minHeight="100vh">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<div>MAIN</div>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/cart" element={<div>CART</div>} />
-        <Route path="/search" element={<div>SEARCH</div>} />
-        <Route path="/category/:genre" element={<div>GENRE</div>} />
-      </Routes>
+      <Box p={'9'} >
+        <Routes>
+          <Route path="/" element={<div>MAIN</div>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<div>CART</div>} />
+          <Route path="/search" element={<div>SEARCH</div>} />
+          <Route path="/category/:genre" element={<div>GENRE</div>} />
+        </Routes>
+      </Box>
+
       <Footer />
     </Flex>
   );
