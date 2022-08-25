dd126084181711753114bcb60b1ba624b7d7eb8a
Luucas0 <luucasdelellis@gmail.com>
2022-08-25 19:16:53 -0300

    cambios

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -9,11 +9,12 @@ function promoteUser(req, res, next) {
   if (req.user.role !== "admin") {
     res.send("You are not an administrator");
   }
+  else{
   User.update({ role: "admin" }, { where: { id: Number(req.params.id) } }).then(
     () => {
       res.send("actualizado");
     }
-  );
+  );}
 }
 //añadida función para quitarle privilegios de ADMIN a un usuario. Imposibilita quitarse los privilegios a uno mismo.
 function demoteUser(req, res, next) {
@@ -22,13 +23,12 @@ function demoteUser(req, res, next) {
   }
   if(req.user.id === Number(req.params.id)){
      res.send("You can't revoke permissions yourself!");
-  }
- 
+  } else{
   User.update({ role: "user" }, { where: { id: Number(req.params.id) } }).then(
     () => {
       res.send("actualizado");
     }
-  );
+  )};
 }
 function deleteUser(req, res, next) {
   if (req.user.role !== "admin") {
