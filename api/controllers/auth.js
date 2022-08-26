56788361eb0ecc92c9098abb515f5450ee3ca10e
unknown <santiposse2000@gmail.com>
2022-08-25 22:52:46 -0300

    Se añade la funcionalidad de eliminar ordenes completas, ahora se pueden eliminar y traer ordenes y productos en estado procesing, corregido error en la ruta para eliminar libros, se quita la posibilidad de que un administrador se elimine a si mismo, además de limpieza general del código y la implementación total del middleware de Admin

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,4 @@
 const User = require("../models/User");
-
-const validateAuth = require("../middleware/auth");
 const { generateToken } = require("../config/token");
 
 class AuthController {
@@ -9,12 +7,13 @@ class AuthController {
       const { password, email } = req.body;
 
       const user = await User.findOne({ where: { email } });
-      if (!user) return res.status(401).send('user not found'); //agregue esta linea p chequear si encontraba bien el usuario
+      if (!user) return res.status(401).send("user not found"); //agregue esta linea p chequear si encontraba bien el usuario
 
       const validation = await user.validatePassword(password); //agregue el await xq nunca validaba el passw
-      if (!validation) return res.status(401).send('password invalid');
+      if (!validation) return res.status(401).send("password invalid");
 
-      const payload = {  //puse el obj en una const para no repetir codigo y mandarlo en la cookie y en el res.send
+      const payload = {
+        //puse el obj en una const para no repetir codigo y mandarlo en la cookie y en el res.send
         id: user.id,
         name: user.name,
         lastname: user.lastname,
@@ -25,7 +24,6 @@ class AuthController {
 
       res.cookie("generatedToken", generateToken(payload));
       res.status(200).send(payload);
-
     } catch (error) {
       res.send(error);
     }
@@ -34,16 +32,12 @@ class AuthController {
   static userSignUp = async (req, res) => {
     User.create(req.body)
       .then((user) => res.sendStatus(201)) //cuando se registra no necesitamos el envio de la data del user xq eos se tiene q cargar en el login
-      
-      .catch((error) => res.send("soy el error", error));
+
+      .catch((error) => res.send("new error", error));
   };
 
   static logOut = (req, res) => {
-
-    res.clearCookie("generatedToken");
-    res.sendStatus(204);
-    //res.cookie("userCreatedToken", "").send("sesión cerrada"); //comente esta linea porq aca estabamos creando una nueva cookie vacia y no estabamos borrando la del usuario logueado
-
+    res.clearCookie("generatedToken").sendStatus(204);
   };
 }
 
--- api/routes/order.js
+++ api/routes/order.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 const { validateAuth } = require("../middleware/auth");
 
-const { continueOrder, getOrder, payOrder, getFullfiled } = require("../controllers/order");
+const {
+  continueOrder,
+  getOrder,
+  payOrder,
+  getFullfiled,
+  deleteOrder,
+} = require("../controllers/order");
 
 router.get("/", validateAuth, getOrder);
+
 router.put("/", validateAuth, continueOrder);
+
 router.put("/pay", validateAuth, payOrder);
+
+router.delete("/:id", validateAuth, deleteOrder);
+
 router.get("/fullfiledOrders", validateAuth, getFullfiled);
 
-module.exports = router
+module.exports = router;
