fc27c44002d1840cb4792c5ee3707e8234d29fa5
franca <francavisokolskis@gmail.com>
2022-08-26 08:21:32 -0300

    validaciones de admin borradas de addbook

--- api/controllers/book.js
+++ api/controllers/book.js
@@ -3,11 +3,6 @@ const { Genre } = require("../models");
 const Book = require("../models/Book");
 
 async function addBook(req, res) {
-
-  if (req.user.role !== "admin") {
-    res.send("You are not an administrator");
-  } 
-
   const book = await Book.create(req.body);
   const genre = await Genre.findByPk(req.body.idGenre);
   genre.addBook(book);
@@ -20,7 +15,7 @@ function updateBook(req, res) {
   });
 }
 function deleteBook(req, res) {
-  Book.destroy({ where : {id: Number(req.params.id)} }).then(() => {
+  Book.destroy({ where: { id: Number(req.params.id) } }).then(() => {
     res.send("Libro eliminado");
   });
 }
