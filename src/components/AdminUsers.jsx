2733b3ffcf06265ed52604addde840c20a92af8c
franca <francavisokolskis@gmail.com>
2022-08-31 11:10:02 -0300

    admin user trae todos los user menos el logueado

--- src/components/AdminUsers.jsx
+++ src/components/AdminUsers.jsx
@@ -19,77 +19,8 @@ import {
     dispatch(getUsers());
   }, [dispatch]); 
 
-/*   //modal
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const initialRef = React.useRef(null);
-  const finalRef = React.useRef(null);
-
-  //form controll
-
-  const {
-    handleSubmit,
-    register,
-    formState: { errors, isSubmitting },
-  } = useForm();
-
-  async function onSubmit(values) {
-    console.log('valuesss', values)
-    await dispatch(addGenre(values));
-    dispatch(getGenres()); 
-    onClose();
-  } */
-
   return (
     <Flex direction={"column"}>
-      {/* <Button mb={"40px"} onClick={onOpen}>
-        Add new genre
-      </Button>
-
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create new genre</ModalHeader>
-          <ModalCloseButton />
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <ModalBody pb={6}>
-
-              <FormControl isInvalid={errors.genreName}>
-                <FormLabel htmlFor="genreName">Genre name</FormLabel>
-                <Input
-                  ref={initialRef}
-                  placeholder="Genre name"
-                  id="genreName"
-                  {...register("genreName", {
-                    required: "This is required",
-                  })}
-                />
-                <FormErrorMessage>
-                  {errors.genreName && errors.genreName.message}
-                </FormErrorMessage>
-              </FormControl>
-
-              <Flex mt={7} justify={"end"}>
-                <Button
-                  colorScheme="blue"
-                  mr={3}
-                  isLoading={isSubmitting}
-                  type="submit"
-                >
-                  Save
-                </Button>
-                <Button onClick={onClose}>Cancel</Button>
-              </Flex>
-            </ModalBody>
-          </form>
-        </ModalContent>
-      </Modal>
- */}
       <Flex direction={"column"}>
         {users.map((user) => (
           <AdminUserItem user={user} key={user.id} />
